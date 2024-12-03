e272fe87468df734d31aa7147c94bb6f79d46f7b
ShahistaSamrin <shaikshahistasamrin@gmail.com>
2024-12-03 15:50:31 +0530

    Add files via upload

---
+++ flippingbits.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class flippingbits{
+    public static void main(String args[]){
+       //flipping bits
+       int a=23;
+       int b=2;
+        System.out.println(a^(2<<b));
+    }
+}
